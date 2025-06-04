3400a16527203144cd80bc388dfb459ad0f3dadc
tharle <tharles.josefidecamargo@gmail.com>
2025-06-03 23:27:21 -0400

    First commit

---
+++ src/main/java/me/tharle/myFirstPlugin/MyFirstPlugin.java
@@ -0,0 +1,17 @@
+package me.tharle.myFirstPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MyFirstPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
