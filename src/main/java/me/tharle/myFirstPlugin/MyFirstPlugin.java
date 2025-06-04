bfc81273edf2c32e9c2dc45044c8dd6c43bf609a
tharle <tharles.josefidecamargo@gmail.com>
2025-06-04 14:26:01 -0400

    added events

--- src/main/java/me/tharle/myFirstPlugin/MyFirstPlugin.java
+++ src/main/java/me/tharle/myFirstPlugin/MyFirstPlugin.java
@@ -1,17 +1,36 @@
 package me.tharle.myFirstPlugin;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class MyFirstPlugin extends JavaPlugin {
+public final class MyFirstPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         getLogger().info("ON ENABLE CUSTOM PLUGIN");
 
+        // Register all events
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         getLogger().info("ON DISABLE CUSTOM PLUGIN");
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        getLogger().info("The player ["+event.getPlayer().getName()+"] has join to the server!!! <3" );
+        event.setJoinMessage("Welcome to the server "+event.getPlayer().getName()+"! Get a chair and relax!");
+    }
+
+    @EventHandler
+    public void onLeaveBed(PlayerBedLeaveEvent event) {
+        Player player = event.getPlayer();
+        player.sendMessage("GOOD MORNING!! ");
     }
 }
